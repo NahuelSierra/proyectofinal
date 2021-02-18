36e74862fd0e9a56a7a31938134826e613dd2129
NahuelSierra <65301559+NahuelSierra@users.noreply.github.com>
2021-02-18 09:49:58 -0300

    Update db.php

--- src/cliente/db.php
+++ src/cliente/db.php
@@ -1,9 +1,9 @@
 <?php
      $conn = mysqli_connect(
-        host: 'ec2-52-6-178-202.compute-1.amazonaws.com',
-        user: 'wnkroxkubwjjkw',
-        password: 'c3277ae0bbc21bf5fd3235d933c384aeefb8960b6fd7ba96ba51e0da327119ae',
-        database: 'd5r2f7ocadc7rb',
-        port: '5432'
+        'sql10.freesqldatabase.com',
+        'sql10393810',
+        'f8jcezeVTS',
+        'sql10393810',
+        '3306'
     );
 ?>

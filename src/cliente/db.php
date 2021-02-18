95276ecb805c693a3018714f36a51518b69b42a2
NahuelSierra <65301559+NahuelSierra@users.noreply.github.com>
2021-02-17 23:54:01 -0300

    Update db.php

--- src/cliente/db.php
+++ src/cliente/db.php
@@ -1,10 +1,12 @@
 <?php
-    $conn = mysqli_connect(
+const mysql = require("mysql");
+    
+    module.exports = mysql_createConnection({
         host: 'remotemysql.com',
         user: 'ziRbxJmt2P',
         password: 'deDkKR7Tyk',
         database: 'ziRbxJmt2P',
         port: '3306',
         dateString: 'date'
-    );
+    });
 ?>
